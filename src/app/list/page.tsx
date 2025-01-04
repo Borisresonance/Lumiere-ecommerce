43bf2f759b61bf2b1752e1a7d7aaecda9718fb01
Borisresonance <llorentejuandiego@gmail.com>
2025-01-04 14:30:13 -0500

    added Category component

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,7 +1,27 @@
-const Page = () => {
-    return(
-        <div className="">Page</div>
-    )
-}
+import Image from "next/image";
 
-export default Page;
+const ListPage = () => {
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      {/* Campaign */}
+      <div className="">
+        <div className="w-2/3">
+          <h1 className="text-3xl font-semibold">
+            Obten 20% de descuento en tu siguiente compra
+          </h1>
+          <button>Compra ahora</button>
+        </div>
+        <div className="relative w-1/3">
+          <Image
+            src="/woman.png"
+            alt="Campaign"
+            fill
+            className="object-contain"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
+import Category from "@/components/Category";
 
 const HomePage = () => {
   return (
@@ -7,6 +8,14 @@ const HomePage = () => {
       <Slider />
       <div className="mt-24 px4 md:px-8 lg:px-16 xl:32 2xl:px-64">
         <h1>Featured Products</h1>
+        <ProductList />
+      </div>
+      <div className="mt-24 px4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+        <h1 className="py-5">Categorias</h1>
+        <Category />
+      </div>
+      <div className="mt-24 px4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+        <h1>Productos Nuevos</h1>
         <ProductList />
       </div>
     </div>
